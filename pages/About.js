bab30bf3ada980bd91b969b2e57fce5a34b272a2
Zakirhusen <72880935+Zakirhusen@users.noreply.github.com>
2022-12-03 11:59:39 +0530

    Double quotes under curly brace

--- pages/About.js
+++ pages/About.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className="my-5 px-10 text-sm sm:text-base  to-blue-200">This website contains mornig and evening authentic duas/supplications from the popular book of authentic supplications, "Hisnul- Muslim" (Fortification of the Muslim). Please Note, transliteration has been added, just for assistance to Muslims who are still learning Arabic. One must not depend on transliteration, and strive to learn arabic. The audio feature has been provided for convenience in the recitation by the user, if needed.
+    <div className="my-5 px-10 text-sm sm:text-base  to-blue-200">This website contains mornig and evening authentic duas/supplications from the popular book of authentic supplications, {"Hisnul- Muslim"} (Fortification of the Muslim). Please Note, transliteration has been added, just for assistance to Muslims who are still learning Arabic. One must not depend on transliteration, and strive to learn arabic. The audio feature has been provided for convenience in the recitation by the user, if needed.
     <p>The site devoloped not for commercial use ,intent of this site spread the awareness of benefits morning and evening azkar.</p>
     <p>If any query or feedback send us email at : <a target="_blank" rel="noreferrer" className="underline text-blue-300" href="https://accounts.google.com/b/0/AddMailService">zakirhusensayed@gmail.com</a></p>
     <p>
