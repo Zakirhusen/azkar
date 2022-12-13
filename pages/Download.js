d23c24ec47d621f1b1d854f3fd7d4b67c6f1f06b
Zakirhusen <72880935+Zakirhusen@users.noreply.github.com>
2022-12-13 20:02:01 +0530

    updating download page

--- pages/Download.js
+++ pages/Download.js
@@ -1,17 +1,141 @@
-import React from 'react'
+import React from "react";
+import {FaEye,FaCloudDownloadAlt} from "react-icons/fa"
 
 const Download = () => {
   return (
-    <div>Download
-    {/* https://api3.islamhouse.com/v3/pav29h2gm56kvlpy/main/get-item/2807299/en/json
-    {/* https://d1.islamhouse.com/data/kn/ih_books/single/kn_hisnul_muslim.pdf */}
-    {/* ಅಲ್ ಉಸೂಲು ಸ್ಸಲಾಸ (ಮೂರು ಮೂಲಭೂತ ನಿಯಮಗಳು)", */}
-{/* "url": "https://d1.islamhouse.com/data/kn/ih_books/single/kn_usool_thalatha.pdf" */} 
-    {/* https://d1.islamhouse.com/data/en/ih_books/single/en_Hisn_El_Muslim.pdf */}
-    {/* https://d1.islamhouse.com/data/ur/ih_books/single2/ur_Hisnul_Muslim.pdf */}
+    <div className="flex flex-col sm:flex-row sm:space-y-0 space-y-5 sm:space-x-5 mx-4 my-8">
+      {/* https://api3.islamhouse.com/v3/pav29h2gm56kvlpy/main/get-item/2807299/en/json
+      {/* https://d1.islamhouse.com/data/kn/ih_books/single/kn_hisnul_muslim.pdf */}
+      {/* ಅಲ್ ಉಸೂಲು ಸ್ಸಲಾಸ (ಮೂರು ಮೂಲಭೂತ ನಿಯಮಗಳು)", */}
+      {/* "url": "https://d1.islamhouse.com/data/kn/ih_books/single/kn_usool_thalatha.pdf" */}
+      {/* https://d1.islamhouse.com/data/en/ih_books/single/en_Hisn_El_Muslim.pdf */}
+      {/* https://d1.islamhouse.com/data/ur/ih_books/single2/ur_Hisnul_Muslim.pdf */}
+      {/* https://d1.islamhouse.com/data/kn/ih_books/single/kn-translation-of-the-meanings-quran.pdf */}
+      <div className="flex  justify-center">
+        <div className="block p-6 rounded-lg shadow-lg bg-white max-w-sm">
+          <h5 className="text-gray-900 text-xl leading-tight font-semibold mb-2">
+            ಹಿಸ್ನುಲ್ ಮುಸ್ಲಿಮ್
+          </h5>
+          <p className="text-gray-700 text-base mb-4"> 
+            Kannada hisnul muslim download in kannada language breif description
+            
+          </p>
+          <div className="flex  space-x-2">
+          <button
+            type="button"
+            className=" flex justify-center"
+          >
+            <a className="text-base " href="https://d1.islamhouse.com/data/kn/ih_books/single/kn_hisnul_muslim.pdf" 
+              target="_blank">
+              <FaCloudDownloadAlt size="1.6rem" color="white" className="button rounded-md p-1 m-0 inline"></FaCloudDownloadAlt>
+            </a>
+          </button>
+          <button
+            type="button"
+            className=" flex justify-center"
+          >
+            <a className="text-base " href="https://d1.islamhouse.com/data/pdf-viewer/web/viewer.html?file=https://d1.islamhouse.com/data/kn/ih_books/single/kn_hisnul_muslim.pdf" 
+              target="_blank">
+              <FaEye size="1.6rem" color="white" className="button rounded-md p-1 m-0 inline"></FaEye>
+            </a>
+          </button>
+          </div>
+        </div>
+      </div>
+      <div className="flex  justify-center">
+        <div className="block p-6 rounded-lg shadow-lg bg-white max-w-sm">
+        <h5 className="text-gray-900 text-xl leading-tight font-medium mb-2">
+            English</h5>
+          <p className="text-gray-700 text-base mb-4">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <div className="flex  space-x-2">
+          <button
+            type="button"
+            className=" flex justify-center"
+          >
+            <a className="text-base " href="https://d1.islamhouse.com/data/en/ih_books/single/en_Hisn_El_Muslim.pdf" 
+              target="_blank">
+              <FaCloudDownloadAlt size="1.6rem" color="white" className="button rounded-md p-1 m-0 inline"></FaCloudDownloadAlt>
+            </a>
+          </button>
+          <button
+            type="button"
+            className=" flex justify-center"
+          >
+            <a className="text-base " href="https://d1.islamhouse.com/data/pdf-viewer/web/viewer.html?file=https://d1.islamhouse.com/data/en/ih_books/single/en_Hisn_El_Muslim.pdf" 
+              target="_blank">
+              <FaEye size="1.6rem" color="white" className="button rounded-md p-1 m-0 inline"></FaEye>
+            </a>
+          </button>
+          </div>
+        </div>
+      </div>
+      <div className="flex  justify-center">
+        <div className="block p-6 rounded-lg shadow-lg bg-white max-w-sm">
+        <h5 className="text-gray-900 text-xl leading-tight font-medium mb-2">
+            حصن املسلم
+          </h5>
+          <p className="text-gray-700 text-base mb-4">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <div className="flex  space-x-2">
+          <button
+            type="button"
+            className=" flex justify-center"
+          >
+            <a className="text-base " href="https://d1.islamhouse.com/data/ur/ih_books/single2/ur_Hisnul_Muslim.pdf" 
+              target="_blank">
+              <FaCloudDownloadAlt size="1.6rem" color="white" className="button rounded-md p-1 m-0 inline"></FaCloudDownloadAlt>
+            </a>
+          </button>
+          <button
+            type="button"
+            className=" flex justify-center"
+          >
+            <a className="text-base " href="https://d1.islamhouse.com/data/pdf-viewer/web/viewer.html?file=https://d1.islamhouse.com/data/ur/ih_books/single2/ur_Hisnul_Muslim.pdf" 
+              target="_blank">
+              <FaEye size="1.6rem" color="white" className="button rounded-md p-1 m-0 inline"></FaEye>
+            </a>
+          </button>
+          </div>
+        </div>
+      </div>
+      <div className="flex  justify-center">
+        <div className="block p-6 rounded-lg shadow-lg bg-white max-w-sm">
+        <h5 className="text-gray-900 text-xl leading-tight font-medium mb-2">
+        Hisnul muslim(hindi)
+          </h5>
+          <p className="text-gray-700 text-base mb-4">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <div className="flex  space-x-2">
+          <button
+            type="button"
+            className=" flex justify-center"
+          >
+            <a className="text-base " href="https://d1.islamhouse.com/data/hi/ih_books/single/hi_hisn_almuslem_1436.pdf" 
+              target="_blank">
+              <FaCloudDownloadAlt size="1.6rem" color="white" className="button rounded-md p-1 m-0 inline"></FaCloudDownloadAlt>
+            </a>
+          </button>
+          <button
+            type="button"
+            className=" flex justify-center"
+          >
+            <a className="text-base " href="https://d1.islamhouse.com/data/pdf-viewer/web/viewer.html?file=https://d1.islamhouse.com/data/hi/ih_books/single/hi_hisn_almuslem_1436.pdf" 
+              target="_blank">
+              <FaEye size="1.6rem" color="white" className="button rounded-md p-1 m-0 inline"></FaEye>
+            </a>
+          </button>
+          </div>
+        </div>
+      </div>
+      
     </div>
-
-  )
-}
-
-export default Download
+  );
+};
+export default Download;
